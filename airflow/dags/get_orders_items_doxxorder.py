f35d2dac416e5cd0a9eab665b14f2f56478d699a
aukrit1993 <aukrit.1993@gmail.com>
2020-07-20 16:14:21 +0700

    IMP formating data and create csv file

--- airflow/dags/get_orders_items_doxxorder.py
+++ airflow/dags/get_orders_items_doxxorder.py
@@ -2,16 +2,9 @@ from datetime import timedelta, datetime
 import requests
 import json
 import pandas as pd
+import re
 
 url = 'http://35.247.175.241'
-
-condition_search = {
-    'searchCriteria[filter_groups][0][filters][0][field]':'created_at',
-    'searchCriteria[filter_groups][0][filters][0][value]':'2020-06-24 03:06:52',
-    'searchCriteria[filter_groups][0][filters][0][condition_type]':'gt',
-    'searchCriteria[page_size]':10000,
-    'searchCriteria[current_page]':1,
-}
 
 print((datetime.today() + timedelta(days=-1) ).strftime("%Y-%m-%d %H:%M:%S"))
 
@@ -26,50 +19,57 @@ def authentication(url):
         
         return response.text.replace('"', '')
 
-def get_data_doxxorder(url, condition_search):
+def get_data_doxxorder(url):
     token = authentication(url)
-    enpoint = '{}/index.php/rest/TH/V1/orders/items'.format(url)
+    # today = (datetime.today() + timedelta(days=-1) ).strftime("%Y-%m-%d")
+    enpoint = '{}/index.php/rest/TH/V1/getOrders?date={}'.format(url, '2020-06-05')
     header = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {}'.format(token),
     }
-    test_get = requests.get(url=enpoint, headers=header, params=condition_search , verify=False)
+    test_get = requests.get(url=enpoint, headers=header, verify=False)
     if test_get.status_code == 200:
         jason_data = json.loads(test_get.text)
-        jason_data = jason_data['items']
-        # print(type(jason_data))
         data = list(map(map_data, jason_data))
-        for i in range(0, len(data)-1): 
-            if data[i] == None:
-                data.pop(i)
-            
-        return data
+        return data[0]
         
 def map_data(data):
-    if data['product_type'] == 'configurable':
-        list_name = data['name'].split('-')
-        color = data['product_option']['extension_attributes']['configurable_item_options'][0]['option_label']
-        size = data['product_option']['extension_attributes']['configurable_item_options'][1]['option_label']
+    if data:
+        return list(map(map_data_items, data['items']))
+    
+def map_data_items(data):
+    if data:
         data_dic = {
-            'order_name': 'doxxorder {}'.format(data['order_id']),
-            'customer_name': 'test',
-            'product_name': list_name[0],
-            'unit_price': '{0:.2f}'.format(float(data['price'])),
-            'qty': data['qty_ordered'],
-            'amount': '{0:.2f}'.format(float(data['row_total'])),
-            'sale_date': datetime.strptime(data['created_at'], '%Y-%m-%d %H:%M:%S'),
+            'customer_name': '',
+            'age': '',
+            'product_name': re.sub(r'\-.*', "", data['name']),
+            'unit_price': round(float(data['price']), 2),
+            'qty': round(float(data['qty']),0),
+            'amount': round(float(data['row_total']), 2),
+            'sale_date': '',
             'channel_sale': 'doxxorder',
-            'category': list_name[1],
-            'brand': 'RD',
-            'color': color,
-            'size': size,
-            'shirt_type': 'M',
+            'category': data['category'],
+            'brand': re.sub(r'\®.*', '', data['brand']),
+            'color': data['color'],
+            'size': data['size'],
+            'shirt_type': '',
+            'seller': '',
         }
+        if 'G' not in str(data_dic['size']).upper():
+            data_dic.update({
+                'shirt_type': 'M'
+            })
+        else:
+            data_dic.update({
+                'shirt_type': 'F'
+            })
         return data_dic
     
-def create_csv():
+def create_csv(url):
+    data = get_data_doxxorder(url)
     
-get_data_doxxorder(url, condition_search)
-
-
-# create table order_item(id serial primary key, order_name char(255), customer_name char(255), product_name char(255), unit_price numeric, qty integer, amount numeric, sale_date timestamp, channel_sale char(255), category char(255), brand char(255), color char(255), size char(10), shirt_type char(2))
+    if data:
+        csv_file = pd.DataFrame(data)
+        csv_file.to_csv('./test_data.csv', index=False)
+    
+create_csv(url)
