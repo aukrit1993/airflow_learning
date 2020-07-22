6a2e7dac98ce569ec612c6b9f6d869df42800c65
aukrit1993 <aukrit.1993@gmail.com>
2020-07-22 18:00:13 +0700

    IMP function create csv

--- airflow/dags/get_orders_items_odoo.py
+++ airflow/dags/get_orders_items_odoo.py
@@ -13,7 +13,7 @@ def authenticate_odoo(url):
         "params":{
             "login":"nong.aukrit@rdbox.co",
             "password":"rd@1234",
-            "db":"20_07_2020"
+            "db":"22_07_2020"
         }
     }
     respones = requests.post(url, json=params)
@@ -21,9 +21,7 @@ def authenticate_odoo(url):
     session_id = json_data['result']['session_id']
     return session_id
     
-# authenticate_odoo(authentication_url)
-    
-def get_data(url):
+def get_data(url, authentication_url):
     session_id = authenticate_odoo(authentication_url)
     headers = {
         'Content-Type': 'application/json',
@@ -36,7 +34,46 @@ def get_data(url):
     }
     respones = requests.post(url, headers=headers, data=json.dumps(params))
     data = json.loads(respones.text)
-    print(data['result'])
+    return data['result']
     
+def clean_data(url, authentication_url):
+    data = get_data(url, authentication_url)
+    if data:
+        data = list(map(map_variants, data))
+        return data
+    
+def map_variants(data):
+    if data['variants']:
+        variants = data['variants']
+        for rec in range(0, len(variants)):
+            try:
+                key_data = list(variants[rec].keys())[0]
+                value_data = list(variants[rec].values())[0]
+                if key_data == 'MODEL':
+                    data.update({
+                        'product_name': value_data,
+                    })
+                elif key_data == 'COLOR':
+                    data.update({
+                        'color': value_data
+                    })
+                elif key_data == 'SIZE':
+                    data.update({
+                        'size': value_data
+                    })
+                elif key_data == 'FG TYPE':
+                    data.update({
+                        'category': value_data
+                    })
+            except:
+                pass
+        del data['variants']
+    else:
+        del data['variants']
+    return data
 
-get_data(url)
+data = clean_data(url, authentication_url)
+
+df = pd.DataFrame(data)
+df.sort_values(by=['product_name'], inplace=True)
+df.to_csv('test.csv', index=False)
